84a39dff4cb7f71e0b8701609d85d5ca61245485
Navyasinghal1234 <write24navya@gmail.com>
2025-11-17 22:02:51 +0530

    q46.c

---
+++ q46.c
@@ -0,0 +1,13 @@
+// WAP TO PRINT THE FOLLOWING PATTERN.                     
+#include <stdio.h>
+
+int main() {
+    for (int i = 1; i <= 5; i++) {        // rows
+        for (int j = 1; j <= 5; j++) {    // columns
+            printf("*");
+        }
+        printf("\n");   // move to next line
+    }
+
+    return 0;
+}
